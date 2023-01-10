6564013d4d3fa75f85842e41f34b4a5fc34933ed
Lucas Praxedes <luk_288@hotmail.com>
2023-01-10 14:46:30 -0300

    feat(component): implement `Header`

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="/favicon.icon" type="image/png" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
