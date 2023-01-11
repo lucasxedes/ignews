fe4e1c95f255394ede35c2879fa355e521dcc00d
Lucas Praxedes <luk_288@hotmail.com>
2023-01-11 13:56:22 -0300

    feat(SSR): implement API `STRIPE`

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface SubScribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubScribeButtonProps) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
